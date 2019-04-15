9fb2c45ba0f25a23976515b4ce88e266371a4927
Abhishek Kumar Gupta <abhishek.gupta1@rivigo.com>
2019-04-15 18:48:53 +0530

    first commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "first-scala-project"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
---
+++ src/main/scala/Demo/HelloScala.scala
@@ -0,0 +1,13 @@
+package Demo
+
+/**
+  * Created by Abhishek gupta on 2019-04-15
+  */
+
+object HelloScala {
+
+  def main(args: Array[String]) {
+    println("Welcome to HelloWorld")
+  }
+
+}
